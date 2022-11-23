7889565ea0bb938af56e4570ad3540a81b0dcb4d
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2022-11-23 17:54:23 -0300

    fileLoader

--- pokedex/src/components/pokemon/Pokemon.jsx
+++ pokedex/src/components/pokemon/Pokemon.jsx
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react"
 import { useFetch } from "../../hook/useFetch"
-import bug from '../../assets/bug.png'
+import Types from "../types/types"
 
 const Pokemon = ({ url }) => {
 
     const { data, loading, error } = useFetch(url)
     const [type, setType] = useState('')
 
-    useEffect(()=>{
+    useEffect(() => {
         data && setType(data['types'][0].type.name)
-    },[data])
+    }, [data])
 
-    
 
-    
+
     return (
         <div className="pok">
             {data && (
                 <>
                     <h1>{data['name']}</h1>
-                    <img className="type" src={bug}/>
+                    <img src="assets/bug.png" alt="typs" />
+                    <div><Types /></div>
                     <div className="flex">
                         <img src={data['sprites'].other.dream_world.front_default} />
                     </div>
